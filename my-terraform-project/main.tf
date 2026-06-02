61d7b706942eb46022752d02fa7cb0855f8b6e0f
dimonalek <dimonalek@gmail.com>
2026-06-02 12:20:07 +0300

    Created first terraform config

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "example2" {
   name                     = "examplestorageacc"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
